19ba321ec5f8295b980d847908ece415e5f1aa7f
unknown <smm45108@gmail.com>
2025-03-24 22:51:26 +0900

    fix: 오타수정

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -65,7 +65,7 @@ router.post("/login", async (req, res) => {
     // 쿠키에 저장
     res.cookie("token", token, {
       httpOnly: true,
-      secure: ture,
+      secure: true,
       sameSite: "None", // 모든 크로스 요청에 허용
     });
     res.json({
