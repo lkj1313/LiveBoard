36d6f8b75c008a4f11ef7f4eba48d859733db222
unknown <smm45108@gmail.com>
2025-03-25 17:43:59 +0900

    refactor : 배포모드로 변경

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,11 +1,11 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
-  autoConnect: false, // 🔥 자동 연결 해제
-});
-// export const socket = io("https://13.125.210.116.nip.io", {
+// export const socket = io("http://localhost:4000", {
 //   autoConnect: false, // 🔥 자동 연결 해제
 // });
+export const socket = io("https://13.125.210.116.nip.io", {
+  autoConnect: false, // 🔥 자동 연결 해제
+});
 export const connectSocket = () => {
   if (!socket.connected) {
     socket.connect(); // ✅ 명시적으로 연결 호출
