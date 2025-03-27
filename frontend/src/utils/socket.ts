40fd1ff3602c00daa44a4e52e36af9ee32c6a3e6
unknown <smm45108@gmail.com>
2025-03-27 22:17:19 +0900

    refactor 서버모드로 변경

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,11 +1,11 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
-  autoConnect: false, //  자동 연결 해제
-});
-// export const socket = io("https://13.125.210.116.nip.io", {
-//   autoConnect: false, // 자동 연결 해제
+// export const socket = io("http://localhost:4000", {
+//   autoConnect: false, //  자동 연결 해제
 // });
+export const socket = io("https://13.125.210.116.nip.io", {
+  autoConnect: false, // 자동 연결 해제
+});
 export const connectSocket = () => {
   if (!socket.connected) {
     socket.connect(); // ✅ 명시적으로 연결 호출
